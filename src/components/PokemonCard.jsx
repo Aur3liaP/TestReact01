58d326641478295ee3fb47a92448379b2425300f
Aur3liaP <pic.aurelia@gmail.com>
2024-06-10 22:43:23 +0200

    dynamic component done !

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,30 @@
-function PokemonCard() {
-  return <figure>
-    <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur"  
-      />
-      <figcaption> Bulbasaur </figcaption>
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+const PokemonCard = () => {
+  let pokemon = pokemonList[0];
+
+  return <figure className="card">
+     {pokemon.imgSrc 
+     ? ( <img className="card-img" src={pokemon.imgSrc} alt={pokemon.name} /> ) 
+     : ( <p>???</p> )
+     } 
+    {/* <img src={pokemon.imgSrc} 
+        alt={pokemon.name}  
+        className="card-img"
+      /> */}
+      <figcaption> {pokemon.name} </figcaption>
   </figure>
 }
+
 
 export default PokemonCard;
