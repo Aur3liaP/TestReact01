525ca9098514c1b7cd29fe148178db7a90024777
Aur3liaP <pic.aurelia@gmail.com>
2024-06-11 09:58:07 +0200

    Test les props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
+const PokemonCard = ({pokemon}) => {
 
-const PokemonCard = () => {
-  let pokemon = pokemonList[0];
 
   return <figure className="card">
      {pokemon.imgSrc 
      ? ( <img className="card-img" src={pokemon.imgSrc} alt={pokemon.name} /> ) 
      : ( <p>???</p> )
      } 
-    {/* <img src={pokemon.imgSrc} 
-        alt={pokemon.name}  
-        className="card-img"
-      /> */}
       <figcaption> {pokemon.name} </figcaption>
   </figure>
 }
